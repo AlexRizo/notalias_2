e7e7e78e91aa3b2a34216ab0b79a283210aa1cbe
devalowee <david_alejandrorizo@outlook.com>
2026-07-22 18:33:56 -0600

    desktop done

--- src/components/react/form/form-sended.tsx
+++ src/components/react/form/form-sended.tsx
@@ -12,6 +12,7 @@ export const FormSended = ({ lang, onClose }: Props) => {
     <div
       className="fixed inset-0 z-60 bg-primary h-screen w-screen"
     >
+      <img src="/assets/icons/notalias.svg" alt="Not Alias" className="m-10 w-36" />
       <div className="rounded-lg h-[90dvh] w-[90dvw] -translate-x-1/2 absolute bottom-5 left-1/2 pb-16 pt-24 lg:size-full lg:px-0 lg:py-0 lg:bottom-[initial] lg:top-0">
         <article className="text-center flex flex-col items-center justify-center gap-16 h-full lg:gap-3">
           <h1 className="text-3xl lg:text-8xl lg:w-100 leading-[0.90]">{ui.contact_page.form_modal.title}</h1>
@@ -19,8 +20,8 @@ export const FormSended = ({ lang, onClose }: Props) => {
             {ui.contact_page.form_modal.description}
           </p>
           <img src="/assets/icons/planet-black.svg" />
-          <button onClick={onClose} className="lg:top-4 lg:right-4 lg:absolute lg:size-4.5">
-            <img src="/assets/icons/plus.svg" className="mt-auto" />
+          <button onClick={onClose} className="lg:top-4 lg:right-10 lg:absolute lg:size-8">
+            <img src="/assets/icons/plus.svg" className="mt-auto brightness-0 size-full" />
           </button>
         </article>
       </div>
