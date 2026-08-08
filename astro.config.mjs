677f467901fe8fba5bb23025a19f01fcf9cbfdd1
alex <alex@rizcode.dev>
2026-08-08 12:14:06 -0600

    privacy policy done

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
   server: {
@@ -15,7 +17,7 @@ export default defineConfig({
     plugins: [tailwindcss()],
   },
 
-  integrations: [react()],
+  integrations: [react(), icon()],
 
   prefetch: {
     defaultStrategy: 'hover',
